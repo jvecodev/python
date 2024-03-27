6bd7982fa01e1054ea953687e8aff07a7e96aaf1
jvEcodev <jvitor.oliveira1803@gmail.com>
2024-03-27 19:37:43 -0300

    atualizei

--- scripts/Novos scripts.py
+++
@@ -1,6 +0,0 @@
-nome = input('Qual é o seu nome?')
-idade =  input ('Qual é a sua idade ? ')
-peso = input ('Qual é o seu peso ?')
-hobby = input ( 'Qual é o seu hobby? ')
-
-print (nome, idade, peso, hobby)

119df9c812f40fd0c55499096c7b4365cbefff2a
jvEcodev <jvitor.oliveira1803@gmail.com>
2024-04-02 21:39:34 -0300

    exercícios-paraiso

---
+++ exe-PUC/ex-raciocinio algorítmico/boxeador.py
@@ -0,0 +1,18 @@
+peso_boxeador = float(input('Digite seu peso por obséquio: '))
+if peso_boxeador < 50:
+    print('Peso palha')
+
+elif peso_boxeador >= 50 and peso_boxeador < 60:
+    print('Peso Pluma')
+
+elif peso_boxeador >=60 and peso_boxeador < 76:
+    print ('Peso Leve')
+
+elif peso_boxeador >= 76 and peso_boxeador < 88:
+    print('Peso Pesado')
+
+else:
+    print('Peso superior ao pesado')   
+     
+
+
---
+++ exe-PUC/ex-raciocinio algorítmico/triangulo.py
@@ -0,0 +1,22 @@
+print ('---DESAFIO TRIÂNGULO---')
+
+lado1 = int(input('Digite o primeiro lado: '))
+lado2 = int(input('Digite o segundo lado: '))
+lado3 = int(input('Digite o terceiro lado: '))
+
+if lado1 <= lado2+lado3 and lado2 <= lado1+lado3 and lado3 <= lado1+lado2:
+    print('Os valores {}, {}, {} formam um triângulo'.format(lado1, lado2, lado3))
+    if lado1 == lado2 and lado2 == lado3 and lado3 == lado1:
+        print ('Triângulo equilátero') 
+    elif lado1 == lado2 or lado1 == lado3 or lado2 == lado3:
+        print ('Triângulo isóceles')
+    elif lado1 != lado2 and lado1 != lado3 and lado2 != lado3:
+        print('Triângulo escaleno')    
+else:
+    print('Não será possível formam um triângulo')
+
+
+
+   
+
+    

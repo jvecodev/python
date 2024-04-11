c7369adde75d3613626ff53d156adb7304f54f62
jvEcodev <jvitor.oliveira1803@gmail.com>
2024-04-11 18:15:05 -0300

    tudo certo

--- ex-guanabara/estruturas de controle/ex.py
+++ ex-guanabara/estruturas de controle/ex.py
@@ -1,29 +1,45 @@
-pergunta2 = 'S'
-pergunta3 = 0
-while pergunta2 == 'S':
-    valor = int(input('Digite um valor: '))
-    pergunta2 = str(input('Quer continuar [S/N]: ')).upper()
-    if pergunta2 != 'S' and pergunta2 != 'N':
-        pergunta3 = str(input('Não compreendo, digite novamente:'))
-        pergunta2 = str(input('Quer continuar [S/N]: ')).upper()
+# pergunta2 = 'S'
+# pergunta3 = 0
+# while pergunta2 == 'S':
+#     valor = int(input('Digite um valor: '))
+#     pergunta2 = str(input('Quer continuar [S/N]: ')).upper()
+#     if pergunta2 != 'S' and pergunta2 != 'N':
+#         pergunta3 = str(input('Não compreendo, digite novamente:'))
+#         pergunta2 = str(input('Quer continuar [S/N]: ')).upper()
        
-print('Fim')
+# print('Fim')
 
 #numero secreto
+
 import random
 computador = (random.randint(1,100))
 contador = 0
+
+numero = 0
 print('='*10)
-while True:
+print ('Desafio')
+print ('='*9)
+while numero != computador:
     numero = int(input('Tente adivinhar o numero de 1 a 100: '))
     if numero < computador:
         print ('Diga um número maior')
-        print('\nTente novamente')
-        contador =+ 1
+        print('Tente novamente')
+        
     elif numero > computador:
         print ('Diga um número menor')
         print('Tente novamente')
-        contador =+ 1
+
     else:
-        print('Parabéns!!')
+        print('Parabéns')
+    contador += 1
+print('Você acertou na {} tentativa'.format(contador))
+
+
+
+
+
+
+
+
+
           

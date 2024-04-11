26cdd6deafefd2b6adbc990c8d1758722bf56107
jvEcodev <jvitor.oliveira1803@gmail.com>
2024-04-11 10:48:26 -0300

    exercicios/CursoEmVideo

--- ex-guanabara/estruturas de controle/novo.py
+++ ex-guanabara/estruturas de controle/novo.py
@@ -1,19 +1,8 @@
-pergunta2 = 'S'
-pergunta3 = 0
-while pergunta2 == 'S':
-    valor = int(input('Digite um valor: '))
-    pergunta2 = str(input('Quer continuar [S/N]: ')).upper()
-    if pergunta2 != 'S' and pergunta2 != 'N':
-        pergunta3 = str(input('Não compreendo, digite novamente:'))
-        pergunta2 = str(input('Quer continuar [S/N]: ')).upper()
-       
-print('Fim')
 
 from random import randint
 computador = randint(0,100)
 contador = 0
 print('==== Desafio ====')
-
 while True:
     pergunta = int(input('\nAcerte um numero de 0 a 100: '))
     if pergunta > computador:
@@ -28,23 +17,3 @@ while True:
         print ('\nParabéns pelo acerto!!!')
 print ('\nAcertou o resultado em {} tentativas'.format(contador))     
 print ('Fim ')   
-
-
-    
-    
-
-               
-
-               
-
-        
-  
-
-
-
-
- 
-
-
-
-    

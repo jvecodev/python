1f8f3746f4010d11dec8aa7fcf8b35990a989a2f
jvEcodev <jvitor.oliveira1803@gmail.com>
2024-04-11 11:46:00 -0300

    reparando

--- ex-guanabara/estruturas de controle/while.py
+++ ex-guanabara/estruturas de controle/while.py
@@ -9,23 +9,8 @@ while pergunta2 == 'S':
        
 print('Fim')
 
-from random import randint
-computador = randint(0,100)
-contador = 0
-print('==== Desafio ====')
 
-while True:
-    pergunta = int(input('\nAcerte um numero de 0 a 100: '))
-    if pergunta > computador:
-        print('\nDiga um número menor ')
-        print('\nTente novamente ')
-        contador =+1
-    if pergunta < computador:
-        print ('\nDiga um número maior ') 
-        print ('\nTenta novamente ')
-        contador =+1
-    else:
-        print ('\nParabéns pelo acerto!!!')
+
 
 
 

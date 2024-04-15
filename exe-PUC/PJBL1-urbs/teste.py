6051858b4855b026179b7616cf2b302eba687892
jvEcodev <jvitor.oliveira1803@gmail.com>
2024-04-15 16:00:19 -0300

    Atualizações finais/ PJBL

--- exe-PUC/PJBL1-urbs/teste.py
+++ exe-PUC/PJBL1-urbs/teste.py
@@ -14,104 +14,112 @@ while True:
     menu_administrador  = True
     
     #abertura
-    
     print('========= Bem-Vindo a Nexus =========')
-    
     print('[1] Usuário')
     print('[2] Administrador')
     print('[3] Sair')
-    # Serviços oferecidos 
+    print ('=-'*25)
     opcao = input('Qual sua opção de acesso ? ').strip()
 
+    # Cadastro
     if opcao == '1':
         nome = str(input('\nDigite seu nome: '))
         print('=-'*30)
         print ('Bem vindo {}, esta é a sua chave de usuário: 121212'.format(nome))
         print('=-'*30) 
+
         #senha digitada
         while menu_usuario:
             chave = input('Digite a chave fornecida: ').strip()
             while contador <=3:
                 if chave == '121212':
+
                     # Entrada permitida
-                    print('=== SERVIÇO AO USUÁRIO ===')
+                    print('\n=== SERVIÇO AO USUÁRIO ===')
                     contador = 0
                     while menu:
                         adicional = True 
                         
                         # Escolha ao menu
-                    
                         print('[1] Usar cartão.')
                         print('[2] Recarga.')
                         print('[3] Sair')
                         print('=-'*30)
-                        opcao_usuario = input('Digite sua opção: ').strip()
+                        opcao_usuario = input('\nDigite sua opção: ').strip()
+
+                        #Usuário decide utilizar o card
                         if opcao_usuario == '1':
                             if saldo < valor_passagem:
-                            
                                 print('SALDO INSUFICIENTE DE: R${}'.format(saldo ))
                                 print('Recarregue imediatamente')
                                 print('=-'*25)
+
+                            #Saldo suficiente0
                             else:
                                 print('Utilizado com sucesso')
                                 saldo -= valor_passagem
-                                print('Saldo de atual com decrescimo = ', saldo)  
+                                print('Saldo  atual com decréscimo = ', saldo) 
+
+                        #Usuário necessita de recarga         
                         elif opcao_usuario == '2':
                             while adicional:
-                                valor_recarga = input('Digite o valor desejado: ')
+                                print ('\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+                                valor_recarga = input('\nDigite o valor desejado: ')
+
+                                #Erro
                                 if valor_recarga.isalpha():
-                                    print('Por favor, números!')
-                                    print('Tente novamente')
+                                    print('\nPor favor, apenas números!')
+                                    print('TENTE NOVAMENTE')
                                     continue
+
+                                #Êxito
                                 if valor_recarga:
                                     adicional = True
-                                    print ('======= Cartões ↓ =======')
+                                    print ('\n======= Cartões ↓ =======')
                                     print('[1] Crédito')
                                     print('[2] Débito')
-                                    formato = input('Qual a forma de pagamento: ')
+                                    formato = input('\nQual a forma de pagamento: ')
+
                                     if formato:
                                         if formato == '1':
-                                            print ('Cartão de crédito')
+                                            print ('\nCartão de crédito')
                                             senha_cartao = input('Digite a senha: ')
                                             print('Conta com senha: ***',senha_cartao)
                                             saldo += float(valor_recarga)
-                                            print('Recarregado com sucesso, saldo atual de ',saldo)
+                                            print('\nRecarregado com sucesso, saldo atual de ',saldo)
                                             adicional = False
 
                                         elif formato == '2':
-                                            print ('Cartão de débito')
+                                            print ('\nCartão de débito')
                                             senha_cartao = input('Digite a senha: ')
                                             print('Conta com senha: ***',senha_cartao)
                                             saldo += float(valor_recarga)
-                                            print('Recarregado com sucesso, saldo atual de ',saldo)
-                                            adicional= False     
-                                            
-                                        else:
-                                            print ('\nCartão não vinculado.')
-                                            print ('Tente novamente')
-                                            print ('=-'*25)
-                                            adicional = False
+                                            print('\nRecarregado com sucesso, saldo atual de ',saldo)
+                                            adicional= False
 
+                                        #Senha alfanumérica negada    
+                                        else:
+                                             if valor_recarga.isalpha():
+                                                print('\nPor favor, apenas números!')
+                                                print('TENTE NOVAMENTE') 
                                             
-                                        
-                                        
                                 else:
                                     print("\nCaracter não identificado.")
                                     print("Tente novamente.")
                                     print("=-"*25)     
-                                continue    
+                                continue   
 
+                        #Usuário decide sair     
                         elif opcao_usuario == '3':
                             print("=-"*25)
                             print('\nVoltando à pagina inicial.')
-                            menu = False
-                            menu_administrador = False
-                            
-                            
-                        
+
+                        #Erro na escolha de opções    
                         else:
                             print('tente novamente')
                             continue   
+
+                # Erro ao digitar a senha        
                 else:
                     contador += 1
                     print("\nSenha incorreta. Tentativas restantes:", 4 - contador)
@@ -120,53 +128,71 @@ while True:
                         print("\nNúmero máximo de tentativas excedido.")
                         break
                 break
-                        
-                        
+
+    #Área administrativa        
     elif opcao == '2':
         nome = str(input('\nDigite seu nome: '))
         print('=-'*30)
         print ('Bem vindo {}, esta é a sua chave de usuário: 212121'.format(nome))
         print('=-'*30)
+
+        # Cadastro
         while menu_administrador:
             chave = input('Digite a chave fornecida: ').strip()
             while contador <=3:
                 if chave == '212121':
+
                     # Entrada permitida
                     print('===== SERVIÇO AO ADMINISTRADOR =====')
                     contador = 0
                     while menu:
                         adicional = True
+
                         # Escolha ao menu
                         print('[1] Alterar valor da passagem: ')
                         print('[2] Ver saldo.')
                         print('[3] Sair')
                         print('=-'*30)
                         opcao_adm = input('Digite sua opção: ').strip()
+
+                        #ALteração de valor
                         if opcao_adm == '1':
-                            nova_passagem = input("\ndigite o valor da passagem")
+                            nova_passagem = input('\nDigite o valor da passagem:- ')
                             if nova_passagem.isnumeric():
                                 valor_passagem = float(nova_passagem)
-                                print('Passagem atual:',valor_passagem)
+                                print('Passagem atual R$',valor_passagem)
+
+                            #Valor alfanumérico    
                             else:
                                 print('Apenas números')
+
+                        #Vizualização de saldo        
                         elif opcao_adm == '2':
                             print ('=-'*25)
                             print('Seu saldo atual: ', saldo)    
-                            print ('=-'*25)    
+                            print ('=-'*25) 
+
+                        #Serviço efetuado       
                         elif opcao_adm == 3:
                             print ('Retornando ao menu administrativo')
-                            menu = False
+                            adicional = False 
                             menu_administrador = False
+                # Erro ao digitar a senha             
                 else:
                     contador +=1
                     print('Erro ao digitar a senha, {}tentativas restantes'.format(3- contador))
                     if contador == 4:
                         menu_usuario = False
-                        print('Numero maximo de tentativas.') 
+                        print('Numero maximo de tentativas.')
+                        break
+                break
+
+    #FIM         
     elif opcao == '3':
-        print("\nSistema encerrado.") 
-        print('=-'*25) 
+        print('\n<SISTEMA ENCERRADO>') 
         print('======== Volte sempre ========')
+
+    #Erro em digitar opções no menu inicial    
     else:
         print('Para prosseguir, apenas opções acimas! ')                     
 
@@ -174,41 +200,41 @@ while True:
 
                         
 
-        
-
     
 
 
 
 
 
-                                   
-                                    
-                                    
+
+
+                                
+                                
+                                
+                    
+                
+
+
+
                         
                     
 
 
 
-                           
-                        
-
-
-
-                    
-
                 
-                    
+
+            
+                
 
 
-
-
-                    
 
 
                 
-                
 
-    
+
+            
+            
+
+
 
 

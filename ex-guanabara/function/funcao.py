0a24013e81c17452905ac36e159f267d44f29a52
jvEcodev <jvitor.oliveira1803@gmail.com>
2024-05-18 12:26:38 -0300

    funcao em python

--- ex-guanabara/function/funcao.py
+++ ex-guanabara/function/funcao.py
@@ -7,18 +7,79 @@
 # area(l, c)
 
 #Escreva
-def escreva(msg):
-    tam = len(msg) 
-    print('~'*tam)
-    print(msg)
-    print('~'*tam)
-frase = str(input('Digite uma frase: '))
-escreva(frase)
-while True:
-    resp = str(input('Quer continuar? [S/N]')).upper()
-    if resp == 'N':
-        break
-    else:
-        frase = str(input('Digite uma frase: '))
-        escreva(frase)
-print('Fim do programa')        
+# def escreva(msg):
+#     tam = len(msg) 
+#     print('~'*tam)
+#     print(msg)
+#     print('~'*tam)
+# frase = str(input('Digite uma frase: '))
+# escreva(frase)
+# while True:
+#     resp = str(input('Quer continuar? [S/N]')).upper()
+#     if resp == 'N':
+#         break
+#     else:
+#         frase = str(input('Digite uma frase: '))
+#         escreva(frase)
+# escreva('Fim do programa')  
+# from time import sleep      
+# inicio = int(input('Digite um número de entrada: '))
+# fim = int(input('Digite um número de saída: '))
+# passo = int(input('Digite o passo: '))
+# for c in range(inicio, fim+1, passo):
+    
+#     print(f'{c} →', end=' ',flush = True)#→Esta nova funcao existe par alternar os intervalos de tempo, funcionando um sleep após cada número.
+#     sleep(0.5)
+# print('{Fim}')
+
+# def maior (*num):
+#     print('Analisando os valores... ')
+#     contador = maior = 0
+#     while True:
+#         contador += 1
+#         n = int(input(f'Digite {contador}° número: '))
+        
+#         if contador == 1:
+#             maior = n
+#         else:
+#             if n > maior:
+#                 maior = n
+#         resp = str(input('Quer continuar? [S/N]')).upper()
+#         if resp == 'N':
+#             break
+#     print(f'Você digitou {contador} números e o maior valor foi {maior}')
+# maior()
+
+from time import sleep
+from random import randint
+
+def sorteia(lista):      
+    print('Sorteando 5 valores da lista: ', end='')
+    for c in range(0, 5):
+        n = randint(1, 10)  
+        lista.append(n)   
+        print(f'{n} ', end='', flush=True)
+        sleep(0.3)
+        
+    print('Pronto!')
+
+def somaPar(lista):
+    soma = 0
+    for valor in lista:
+        if valor %2==0:
+            soma += valor
+    print(f'Somando os valores pares de {lista}, temos {soma}')
+
+numeros = list()
+sorteia(numeros)
+somaPar(numeros)
+
+
+    
+    
+
+
+            
+
+ 
+

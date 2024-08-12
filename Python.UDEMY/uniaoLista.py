f76acd28821a71e126c9e8da851ed2ddfeb9befc
jvecodev <jvitor.oliveira1803@gmail.com>
2024-08-11 22:13:42 -0300

    Primeira alteração feita no terminal

--- Python.UDEMY/uniaoLista.py
+++ Python.UDEMY/uniaoLista.py
@@ -40,7 +40,7 @@ lista_b = [12, 2, 3, 6, 50, 60, 70]
 lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
 print(lista_soma)  # [22, 4, 6, 10, 55, 60, 70]
 
-#Teste para git
+
 
 
 

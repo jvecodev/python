b4aea4cdb5f20eac3df45e9d33ea9db023fea2c2
jvecodev <jvitor.oliveira1803@gmail.com>
2024-08-11 22:27:04 -0300

    Commit git

--- Python.UDEMY/uniaoLista.py
+++ Python.UDEMY/uniaoLista.py
@@ -38,9 +38,8 @@ from itertools import zip_longest
 lista_a = [10, 2, 3, 4, 5]
 lista_b = [12, 2, 3, 6, 50, 60, 70]
 lista_soma = [x + y for x, y in zip_longest(lista_a, lista_b, fillvalue=0)]
-print(lista_soma)  # [22, 4, 6, 10, 55, 60, 70]
+print(lista_soma)  
 
-# teste pelo terminal
 
 
 
